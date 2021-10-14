a47f2dad7e80c0fa67d522292b2377dcfefdeccc
ptsuardana01 <ptsuardana01@gmail.com>
2021-10-14 13:55:19 +0800

    Test Commit2

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-admin-dashboard>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Dashboards
+            Dashboard
         </h2>
     </x-slot>
 
